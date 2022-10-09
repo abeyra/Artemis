bf8b7561a0e516440adaf034f42dc53d7e70acf1
andres beyra <andresbeyra@gmail.com>
2022-10-09 11:17:03 -0400

    added netlify.toml file to help with redirects and solve netlify page not found issue. removed url variable from navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,6 @@ import './Navbar.scss';
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Link } from '@mui/material';
 import RocketIcon from '@mui/icons-material/Rocket';
 
-const url = 'https://clever-dasik-399063.netlify.app/';
-
 const Navbar = () => {
   return (
     <AppBar position="static" style={{ background: '#282828'}}>
@@ -19,7 +17,7 @@ const Navbar = () => {
               Space App
             </Typography>
             <Stack direction="row" spacing={2}>
-                <Button color="inherit" href={url + `stars`}>Learning with Nasa</Button>
+                <Button color="inherit" href="/stars">Learning with Nasa</Button>
                 <Button color="inherit" href="#cataclysmic">Cataclysmic Variables</Button>
                 <Button color="inherit" href="#cepheids">Cepheids</Button>
                 <Button color="inherit" href="#lightcurves">Light Curves</Button>
