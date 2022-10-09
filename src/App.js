85f718f03ad3c30640dd63dd13b57a50deaa9d97
andres beyra <andresbeyra@gmail.com>
2022-10-09 13:07:04 -0400

    updated import route for css into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './reset.css';
-import './App.scss';
+import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Stars from './pages/Stars/Stars.jsx';
