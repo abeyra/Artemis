fa43ce7fd4c730e5fe33687d5b535e77c0370da0
andres beyra <andresbeyra@gmail.com>
2022-10-09 10:32:46 -0400

    trying to fix route to information page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import './Navbar.scss';
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Link } from '@mui/material';
 import RocketIcon from '@mui/icons-material/Rocket';
 
+const url = 'https://clever-dasik-399063.netlify.app/';
+
 const Navbar = () => {
   return (
     <AppBar position="static" style={{ background: '#282828'}}>
@@ -17,7 +19,7 @@ const Navbar = () => {
               Space App
             </Typography>
             <Stack direction="row" spacing={2}>
-                <Button color="inherit" href="/stars">Learning with Nasa</Button>
+                <Button color="inherit" href={url + `stars`}>Learning with Nasa</Button>
                 <Button color="inherit" href="#cataclysmic">Cataclysmic Variables</Button>
                 <Button color="inherit" href="#cepheids">Cepheids</Button>
                 <Button color="inherit" href="#lightcurves">Light Curves</Button>
